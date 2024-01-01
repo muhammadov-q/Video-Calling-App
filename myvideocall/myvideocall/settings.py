39f0d53c6568c026428b2833d6bbfaf1ecd33243
Qobiljon Muhammadov <muhammadov_q@auca.kg>
2024-01-02 02:09:47 +0600

    Finish user registration

--- myvideocall/myvideocall/settings.py
+++ myvideocall/myvideocall/settings.py
@@ -113,10 +113,14 @@ USE_I18N = True
 USE_TZ = True
 
 
-# Static files (CSS, JavaScript, Images)
+# Static files (css, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
 STATIC_URL = 'static/'
+
+STATICFILES_DIRS = [
+    BASE_DIR / "vibecall/static",
+]
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
